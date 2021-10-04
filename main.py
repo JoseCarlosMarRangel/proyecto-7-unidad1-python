75d5f424d05dbb4f4a8adcb84a1d478db1f0f856
AlfonsoAHR <56330213+AlfonsoAHR@users.noreply.github.com>
2021-10-03 21:06:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,10 @@ class calculo(QtWidgets.QWidget):
 
         self.si10.setEnabled(False)
         self.no10.setEnabled(False)
+
+        self.salir = QtWidgets.QPushButton("Salir")
+
+        self.salir.setEnabled(False)
         
     
 
@@ -116,6 +120,10 @@ class calculo(QtWidgets.QWidget):
         grid.addWidget(self.si10,19,0)
         grid.addWidget(self.no10,19,1)
 
+        grid.addWidget(self.respuesta,21,0)
+
+        grid.addWidget(self.salir,21,2)
+
         grid.addWidget(QtWidgets.QLabel("Le duele la cabeza?"),0,0)
         grid.addWidget(QtWidgets.QLabel("Tiene dolor de espalda?"),2,0)
         grid.addWidget(QtWidgets.QLabel("Tiene ojos cansados regularmente?"),4,0)
@@ -129,13 +137,29 @@ class calculo(QtWidgets.QWidget):
                 
         grid.addWidget(QtWidgets.QLabel("Enfermedad"),20,0)
 
-        self.si.clicked.connect(self.activar1)
-        self.si2.clicked.connect(self.activar3)
-        self.si3.clicked.connect(self.activar4)
-        self.no.clicked.connect(self.activar2)
+        self.si.clicked.connect(self.activar2)
+        self.si2.clicked.connect(self.activar2)
+        self.si3.clicked.connect(self.terminar1)
+        self.si4.clicked.connect(self.activar4)
+        self.si5.clicked.connect(self.terminar2)
+        self.si6.clicked.connect(self.activar7)
+        self.si7.clicked.connect(self.activar7)
+        self.si8.clicked.connect(self.activar4)
+        self.si9.clicked.connect(self.terminar4)
+        self.si10.clicked.connect(self.terminar5)
         
+        self.no.clicked.connect(self.activar5)
+        self.no2.clicked.connect(self.activar2)
+        self.no3.clicked.connect(self.activar3)
+        self.no4.clicked.connect(self.terminar3)
+        self.no5.clicked.connect(self.activar8)
+        self.no6.clicked.connect(self.activar4)
+        self.no7.clicked.connect(self.activar7)
+        self.no8.clicked.connect(self.activar9)
+        self.no9.clicked.connect(self.activar9)
+        self.no10.clicked.connect(self.terminar3)
 
-       
+        self.salir.clicked.connect(self.cerrar)
 
         self.show()
 
@@ -175,13 +199,42 @@ class calculo(QtWidgets.QWidget):
     def activar8(self):
         self.si9.setEnabled(True)
         self.no9.setEnabled(True)
+        self.respuesta.setText("Probablemente Alzhaimer")
+        self.salir.setEnabled(True)
 
     def activar9(self):
         self.si10.setEnabled(True)
-        self.si10.setEnabled(True)
+        self.no10.setEnabled(True)
+
+
+
+
+    def terminar1(self):
+        self.respuesta.setText("Epilepsia")
+        self.salir.setEnabled(True)
+
+    def terminar2(self):
+        self.respuesta.setText("Sinusitis o hipotermia")
+        self.salir.setEnabled(True)
+
+    def terminar3(self):
+        self.respuesta.setText("Covid o Asma")
+        self.salir.setEnabled(True)
+
+    def terminar4(self):
+        self.respuesta.setText("Ictericia")
+        self.salir.setEnabled(True)
+
+    def terminar5(self):
+        self.respuesta.setText("Golpe de calor o Diabetes")
+        self.salir.setEnabled(True)
+
         
+
+
+    def cerrar(self):
+         self.salir.clicked.connect(self.close)
 
 calc = calculo()
 
 sys.exit(app.exec_())
-
